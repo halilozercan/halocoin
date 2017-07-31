9ef0e6fcb1780322cafd94ce3289de96bebc17a1
Halil Ozercan <halilozercan@gmail.com>
2017-07-31 13:17:43 +0200

    0.0.2

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import find_packages
 
 setup(
     name='Halocoin',
-    version='0.0.1',
+    version='0.0.2',
     description='An educational blockchain implementation. Forked from basiccoin',
     author='Halil Ozercan',
     author_email='halilozercan@gmail.com',
     url='https://github.com/halilozercan/halocoin',
-    download_url='https://github.com/halilozercan/halocoin/tarball/0.0.1',
+    download_url='https://github.com/halilozercan/halocoin/tarball/0.0.2',
     entry_points={
         'console_scripts': [
             'halocoin = halocoin.cli:main',
