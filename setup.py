f8fd4082e172b99c62d4f4c331634cec9af8441b
Halil Ozercan <halilozercan@gmail.com>
2017-12-27 16:04:02 +0300

    setup.py updated for coinami branch

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name='Halocoin',
-    version='0.1.0.4',
-    description='An educational blockchain implementation. Forked from zack-bitcoin/basiccoin',
-    author='Halil Ozercan',
+    name='Coinami',
+    version='0.0.1',
+    description='Read mapping incentivized cryptocurrency. Branch of halilozercan/halocoin',
+    author='H. Ibrahim Ozercan',
     author_email='halilozercan@gmail.com',
     url='https://github.com/halilozercan/halocoin',
     entry_points={
         'console_scripts': [
-            'halocoin = halocoin.cli:main'
+            'coinamid = halocoin.cli:main'
         ],
     },
     include_package_data=True,
     install_requires=['requests', 'wheel', 'pyyaml', 'flask', 'flask-socketio',
                       'pycrypto', 'm3-cdecimal', 'simplekv',
-                      'werkzeug', 'json-rpc', 'tabulate', 'SQLAlchemy', 'ecdsa'],
+                      'werkzeug', 'tabulate', 'SQLAlchemy', 'ecdsa'],
     packages=find_packages(exclude=("tests", "tests.*")),
 )
