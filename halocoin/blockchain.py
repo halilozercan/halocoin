34a208e2632e0028a39159b73539cac15aa69f4d
Halil Ozercan <halilozercan@gmail.com>
2018-01-20 12:31:08 +0300

    0.0009c lock database

--- halocoin/blockchain.py
+++ halocoin/blockchain.py
@@ -410,7 +410,7 @@ class BlockchainService(Service):
                 return Response(False, 'Reward or spend transactions must be addressed')
             if not BlockchainService.tx_signature_check(tx):
                 return Response(False, 'Transaction is not properly signed')
-            if tools.is_address_valid(tx['to']):
+            if not tools.is_address_valid(tx['to']):
                 return Response(False, 'Address is not valid')
             if tx['to'] == tools.tx_owner_address(tx):
                 return Response(False, 'You cannot transfer money to same account!')
