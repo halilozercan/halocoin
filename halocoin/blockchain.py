e40b3067020b72a3b0213dfed8b563df40f415c7
Halil Ozercan <halilozercan@gmail.com>
2018-01-30 10:49:53 +0300

    miners included false block

--- halocoin/blockchain.py
+++ halocoin/blockchain.py
@@ -69,25 +69,22 @@ class BlockchainService(Service):
                             return
 
                     self.db.simulate()
-                    try:
-                        length = self.db.get('length')
-                        for i in range(20):
-                            block = self.get_block(length)
-                            if self.fork_check(blocks, length, block):
-                                self.delete_block()
-                                length -= 1
-                            else:
-                                break
+                    length = self.db.get('length')
+                    for i in range(20):
+                        block = self.get_block(length)
+                        if self.fork_check(blocks, length, block):
+                            self.delete_block()
+                            length -= 1
+                        else:
+                            break
 
-                        for block in blocks:
-                            add_block_result = self.add_block(block)
-                            if add_block_result == 2:  # A block that is ahead of us could not be added. No need to proceed.
-                                break
-                            elif add_block_result == 0:
-                                total_number_of_blocks_added += 1
-                                api.new_block()
-                    except Exception as e:
-                        print("halo")
+                    for block in blocks:
+                        add_block_result = self.add_block(block)
+                        if add_block_result == 2:  # A block that is ahead of us could not be added. No need to proceed.
+                            break
+                        elif add_block_result == 0:
+                            total_number_of_blocks_added += 1
+                            api.new_block()
 
                     if total_number_of_blocks_added == 0 or self.db.get('length') != blocks[-1]['length']:
                         # All received blocks failed. Punish the peer by lowering rank.
@@ -194,7 +191,10 @@ class BlockchainService(Service):
         elif int(block['length']) > int(length) + 1:
             return 2
 
-        #tools.echo('add block: ' + str(block['length']))
+        if block['length'] == 909:
+            print("Here we are")
+
+        tools.echo('add block: ' + str(block['length']))
 
         if (length >= 0 and block['diffLength'] != tools.hex_sum(block_at_length['diffLength'],
                                                                  tools.hex_invert(block['target']))) \
@@ -256,7 +256,7 @@ class BlockchainService(Service):
         for orphan in sorted(orphans, key=lambda x: x['count'] if 'count' in x else -1):
             self.tx_queue.put(orphan)
 
-        #tools.techo('add block: ' + str(block['length']))
+        tools.techo('add block: ' + str(block['length']))
         return 0
 
     def delete_block(self):
