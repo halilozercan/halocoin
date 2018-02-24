883a79dc9d873cc5562384b59801aae6bd2b2891
Halil Ozercan <halilozercan@gmail.com>
2018-02-24 14:12:55 +0300

    power2

--- coinami/executor.sh
+++ coinami/executor.sh
@@ -1,2 +1,32 @@
-#!/usr/bin/env bash
-rabix --quiet coinami.cwl /input/coinami.job.json
+#!/bin/bash
+cd /input
+
+echo "Starting decompressing..."
+decompress job.cfq
+echo "Finished decompressing..."
+
+num_of_threads=$(python3 -c "import json; a=json.load(open('coinami.job.json')); print(a['threads'])")
+
+echo "Starting BWA..."
+bwa mem -t $num_of_threads /reference/human_g1k_v37.fasta reads.1.fq reads.2.fq > output.sam
+echo "Finished bwa..."\n
+
+echo "Starting Samtools View..."
+samtools view -@ $num_of_threads -bS -o output.bam output.sam
+echo "Finished Samtools View..."
+
+echo "Starting Samtools Sort..."
+samtools sort -@ $num_of_threads output.bam -o output.sorted
+echo "Finished Samtools Sort..."
+
+echo "Starting MarkDuplicates..."
+java -jar /usr/local/bin/picard.jar MarkDuplicates I=output.sorted O=output.markdup.bam M=duplicate_metrics.txt
+echo "Finished MarkDuplicates..."
+
+echo "Starting Samtools Index..."
+samtools index -@ $num_of_threads output.markdup.bam
+echo "Finished Samtools Index..."
+
+echo "Starting Zip..."
+zip result.zip output.markdup.bam output.markdup.bam.bai
+echo "Finished Zip..."
